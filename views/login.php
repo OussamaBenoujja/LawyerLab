40607c953f6c72bc8fde66acad2087d2ee93cea5
OussamaBenoujja <osama2code79@gmail.com>
2024-12-17 10:34:17 +0100

    added views files for the site template

---
+++ views/login.php
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOG IN</title>
+</head>
+<body>
+    
+<nav>
+
+</nav>
+
+<main>
+    <form>
+        <label for='email'>Email</label>
+        <input type='email' name='email'>
+        <label for='password'>Password</label>
+        <input type='password' name='password'>  
+        <button type="submit">Log In</button>
+    </form>
+</main>
+
+<footer>
+</footer>
+
+</body>
+</html>
